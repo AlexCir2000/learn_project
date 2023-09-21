5bba9b7a3a6668222049dc25710727cf301d76e6
Alex <Alex@mail.ru>
2023-09-21 16:17:01 +0300

    readme changed

--- test.py
+++ test.py
@@ -1,25 +1,30 @@
 from random import randint
-from datetime import datetime
+from datetime import datetime as dt
 
 
 def bubble_sort(spisok):
     """Function Функция сортировки
     пузырьковым методом."""
-    for i in range(0, len(spisok)-1):
-        for j in range(0, len(spisok)-1):
-            if spisok[j] > spisok[j+1]:
-                temp = spisok[j+1]
-                spisok[j+1] = spisok[j]
+    for i in range(0, len(spisok) - 1):
+        for j in range(0, len(spisok) - 1):
+            if spisok[j] > spisok[j + 1]:
+                temp = spisok[j + 1]
+                spisok[j + 1] = spisok[j]
                 spisok[j] = temp
     return spisok
 
 
-N = 10
+
+
+
+
+N = 10  # TODO Rename variable
 spis = []
-for k in range(0, N-1):
+for k in range(0, N - 1):
     spis.append(randint(0, 100))
 
 print(spis)
-T1 = datetime.now()
+T1 = dt.now()
 sorted_spis = bubble_sort(spis)
-print(datetime.now() - T1)
+print(dt.now() - T1)
+
